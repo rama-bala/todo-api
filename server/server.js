2b0a5a9383566e3b8b559e48a6ea1540a70ae548
Rama Bala <ramabala.java@gmail.com>
2018-02-15 22:24:35 -0500

    added middleware to auththenticate prive get user route

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./model/todo');
 var { User } = require('./model/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -127,6 +128,13 @@ app.post('/users', (req,res) => {
         res.status(400).send(e);
     });
 })
+
+
+
+
+app.get('/users/me' ,authenticate, (req,res) => {
+    res.send(req.user); 
+});
 
 app.listen(port, () => {
     console.log(`Server started listening on port ${port}`);
